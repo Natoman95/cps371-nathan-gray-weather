0aed719b83cbe01d925d53820adb1d00b20bf22d
Nathan Gray <nathan.gray@gordon.edu>
2016-02-21 23:51:43 -0500

    RESTful Server Modifications
    Adapted the Angular to work with the much smaller weather JSON that is
    now processed and cached on a server.
    
    Added a proxy so that it’s possible for the app to make requests
    outside of its own domain when testing in the web browser.

--- Weather App/www/js/WeatherDataCtrl.js
+++ Weather App/www/js/WeatherDataCtrl.js
@@ -1,71 +1,34 @@
 app.controller('WeatherDataCtrl', function ($scope, weatherService, picService) {
   // Get weather data
-  var weather = weatherService.getData().then(function (response) {
-    weather = response.data;
-    console.dir(weather);
+  $scope.weather = weatherService.getData().then(function (response) {
+      $scope.weather = response.data;
+      console.dir($scope.weather);
 
-    // Get current temp
-    $scope.getCurrentTemp = function () {
-      return weather.currently.temperature;
-    };
+      // Get the date
+      $scope.getDate = function (day) {
+        return new Date($scope.weather.daily[day].time * 1000);
+      };
 
-    // Get the day's dew point
-    $scope.getDewPoint = function (day) {
-      if (day === 0) {
-        return weather.currently.dewPoint;
-      }
-      return weather.daily.data[day].dewPoint;
-    };
+      // Get the pic for the day's temp
+      $scope.getTempPic = function (day) {
+        if (day === 0) {
+          return picService.getTempPic($scope.weather.currently.temperature);
+        }
+        var avgTemp = ($scope.weather.daily[day].maxTemp +
+          $scope.weather.daily[day].maxTemp) / 2;
+        return picService.getTempPic(avgTemp);
+      };
 
-    // Get the day's humidity
-    $scope.getHumidity = function (day) {
-      if (day === 0) {
-        return weather.currently.humidity;
-      }
-      return weather.daily.data[day].humidity;
-    };
+      // Get the pic for the weather summary icon
+      $scope.getIconPic = function (day) {
+        if (day === 0) {
+          return picService.getIconPic($scope.weather.currently.icon);
+        }
+        return picService.getIconPic($scope.weather.daily[day].icon);
+      };
 
-    // Get the day's summary
-    $scope.getSummary = function (day) {
-      if (day === 0) {
-        return weather.currently.summary;
-      }
-      return weather.daily.data[day].summary;
-    };
-
-    // Get the day's max temp
-    $scope.getMaxTemp = function (day) {
-      return weather.daily.data[day].temperatureMax;
-    };
-
-    // Get the day's min temp
-    $scope.getMinTemp = function (day) {
-      return weather.daily.data[day].temperatureMin;
-    };
-
-    // Get the date
-    $scope.getDate = function (day) {
-      return new Date(weather.daily.data[day].time * 1000);
-    };
-
-    // Get the pic for the day's temp
-    $scope.getTempPic = function (day) {
-      if (day === 0) {
-        return picService.getTempPic($scope.getCurrentTemp());
-      }
-      var avgTemp = ($scope.getMaxTemp(day) + $scope.getMinTemp(day)) / 2;
-      return picService.getTempPic(avgTemp);
-    };
-
-    // Get the pic for the weather summary icon
-    $scope.getIconPic = function (day) {
-      if (day === 0) {
-        return picService.getIconPic(weather.currently.icon);
-      }
-      return picService.getIconPic(weather.daily.data[day].icon);
-    };
-
-  }, function (error) {
-    console.error("Error getting data!");
-  });
+    },
+    function (error) {
+      console.error("Error getting data!");
+    });
 });
--- Weather App/www/js/weatherService.js
+++ Weather App/www/js/weatherService.js
@@ -1,15 +1,10 @@
 // Code Adam used in his demo in class
 app.service("weatherService", function ($http) {
-  var API_KEY = "10e3cd1e90ea7df35786186d55eca4ec"; // INSERT API KEY HERE
-  var COORDS = {
-    lat: "42.589611", // INSERT LATITUDE HERE
-    long: "-70.819806" // INSERT LONGITUDE HERE
-  };
-  var URL = "https://api.forecast.io/forecast/" +
-            API_KEY + "/" + COORDS.lat + "," + COORDS.long
-            + "?callback=JSON_CALLBACK"; // Request JSON-P response
+  var zip = "01984";
+  var proxyURL = "/app-server/";
+  var fullURL = proxyURL + "?zip=" + zip;
 
   this.getData = function () {
-      return $http.jsonp(URL);
+    return $http.get(fullURL);
   };
 });
